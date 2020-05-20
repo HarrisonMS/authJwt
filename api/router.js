8f549ceb65cadc4f09a52e8e906d81ea9adcd101
Harrison <hmseaborn@gmail.com>
2020-05-19 18:13:15 -0700

    mvpish

--- api/router.js
+++ api/router.js
@@ -1,5 +1,9 @@
 const router = require("express").Router();
+const authRouter = require("../auth/router");
+const usersRouter = require("../users/router");
 
+router.use("/users", usersRouter);
+router.use("/auth", authRouter);
 router.get("/", (req, res) => {
   res.json({ api: "You figured it out!" });
 });
---
+++ auth/restrictedMid.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+const secrets = require("../api//vars");
+
+module.exports = (req, res, next) => {
+  // tokens are normally sent as the Authorization header
+  const token = req.headers.authorization;
+
+  const secret = secrets.jwtSecret;
+
+  if (token) {
+    // verify that the token is valid
+    jwt.verify(token, secret, (error, decodedToken) => {
+      // if everything is good with the token, the error will be undefined
+      if (error) {
+        res
+          .status(401)
+          .json({ you: "you dont have the correct credentials to do that" });
+      } else {
+        req.jwt = decodedToken;
+
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ message: "Please provide credentials" });
+  }
+};
